3165f39c5df3e7a1468539a4846d0552f5a49813
ccoropuna123 <74697970@continental.edu.pe>
2026-05-11 00:04:01 -0500

    Agrego botón de reporte en dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <div class="text-gray-900">
+                        ¡Has iniciado sesión! ✔
+
+                        <br><br>
+
+                        <a href="{{ route('reports.create') }}"
+                        class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700">
+                            Crear reporte de incendio
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
