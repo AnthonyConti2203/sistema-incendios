09aec58bddbcff520e38302f6e282bee0f82d2d9
ccoropuna123 <74697970@continental.edu.pe>
2026-06-01 01:48:16 -0500

    User
    comentarios para el php User

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,9 +9,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
+//crea la clase user
+//"extends authenticatable " hereda todo lo del sistema
+//de autenticación de Laravel (login, logout, sesiones)
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+    // Activa dos funcionalidades dentro de la clase:
+    // HasFactory  → permite crear usuarios falsos para pruebas
+    // Notifiable  → permite enviar notificaciones al usuario
 
     /**
      * The attributes that are mass assignable.
@@ -19,6 +25,10 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+    // Define qué campos se pueden llenar masivamente
+    // "Masivamente" = cuando usas User::create([...])
+    // Solo los campos listados aquí pueden guardarse así
+    // Es una medida de SEGURIDAD
         'name',
         'email',
         'password',
@@ -35,6 +45,8 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
+        // La contraseña NUNCA se muestra en respuestas JSON
+        // Aunque alguien intercepte la respuesta, no verá la contraseña
         'remember_token',
     ];
 
@@ -42,12 +54,18 @@ class User extends Authenticatable
     public function reports()
     {
         return $this->hasMany(Report::class);
+        // 'hasMany' = "tiene muchos"
+        // Le dice a Laravel: busca en la tabla 'reports'
+        // todos los registros donde user_id = este usuario
     }
 
     // Un usuario puede tener muchas notificaciones
     public function notifications()
     {
         return $this->hasMany(Notification::class);
+        // Igual que reports pero con la tabla 'notifications'
+        // Busca todas las notificaciones donde user_id = este usuario
+        
     }
 
     /**
@@ -56,10 +74,18 @@ class User extends Authenticatable
      * @return array<string, string>
      */
     protected function casts(): array
+    //el casts convierte automaticamnete los datos cuando los guardan o 
+    //lees de la BD
     {
         return [
             'email_verified_at' => 'datetime',
+            //el email_verified guarda la fecha y la hora en 
+            //el que el usuario verifico el correo
+
+            //el datatime lo convierte a un objeto fecha
             'password' => 'hashed',
+            //en esta linea cuando guardas una contraseña , laravel la encripta
+            //automaticamente
         ];
     }
 }
